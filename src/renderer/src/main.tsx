776886fa40ff052c855371b462dcb7da6b079a2a
Théo Armengou <armengou.th@outlook.fr>
2023-10-19 19:58:17 +0200

    Added redux

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App/App'
+import { Provider } from 'react-redux'
+import { store } from './Store/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
